bb9e148cb9f4229a17446c764a4cfda815b45e0f
JP <juanpablocapilla@gmail.com>
2022-04-27 14:45:22 -0300

    Layout

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Layout from './Screens/Layout';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text><h1>Hola Coder! - Primer trabajo practico</h1>></Text>
-      <StatusBar style="auto" />
-    </View>
+    <Layout/>
   );
 }
 
---
+++ Screens/Layout.js
@@ -0,0 +1,20 @@
+
+import { View, Button, TextInput, Text } from "react-native";
+
+const Layout = () => {
+    return(
+        <View>
+            <View>
+                <TextInput>
+
+                </TextInput>
+                <Button title='Add Task' />
+            </View>
+            <Text>
+                Layout text!
+            </Text>
+        </View>
+    )
+}
+
+export default Layout;
