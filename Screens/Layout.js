44d1895a8766bae3751b7c8d8137ca83578ce75c
JP <juanpablocapilla@gmail.com>
2022-04-27 16:31:55 -0300

    screens & colors palet

--- Screens/Layout.js
+++ Screens/Layout.js
@@ -1,20 +1,47 @@
 
-import { View, Button, TextInput, Text } from "react-native";
+import { View, Button, TextInput, Text, StyleSheet } from "react-native";
+import { colors } from "../Styles/Colors";
+import Item from "../Components/Item";
 
 const Layout = () => {
     return(
-        <View>
-            <View>
-                <TextInput>
-
-                </TextInput>
+        <View style={styles.container}>
+            <View style={styles.topContainer}>
+                <TextInput style={styles.input} />
                 <Button title='Add Task' />
             </View>
-            <Text>
-                Layout text!
-            </Text>
+            <View style={styles.itemList}>
+                <Item item={{id:1, todo: "Estudiar React Native!"}}></Item>
+            </View>
         </View>
     )
 }
 
 export default Layout;
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 25,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    topContainer:{
+        marginTop:30,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 8, 
+    },
+    input:{
+        borderRadius: 8,
+        borderWidth: 2,
+        width: 200,
+        padding: 4,
+    },
+    itemList:{
+        backgroundColor: colors.gray,
+        borderRadius: 8,
+        width: '90%',
+        padding: 20,
+    }
+})
---
+++ Styles/Colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    gray: '#e5e5e5',
+    white: '#FCFFE7',
+    celeste: '#4cc9f0',
+    azul: '#4361ee',
+    violeta: '#7209b7',
+    rosa: '#f72585',
+    black: '000000',
+}
